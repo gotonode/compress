7ee29155b4cf4e2b7ad2b4af4c40ab63ac906556
gotonode <5214152+gotonode@users.noreply.github.com>
2019-01-19 15:18:06 +0200

    Project is taking shape

---
+++ src/main/java/io/github/gotonode/compress/Main.java
@@ -0,0 +1,10 @@
+package main.java.io.github.gotonode.compress;
+
+public class Main {
+
+    public static final String APP_NAME = "Compress";
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to " + APP_NAME + "!");
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,27 @@
+package test.java;
+
+import main.java.io.github.gotonode.compress.Main;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    /**
+     * A sample test.
+     */
+    @Test
+    public void genericTest() {
+        assertTrue(true);
+    }
+
+    /**
+     * Tests that the app's name is correct.
+     */
+    @Test
+    public void testAppName() {
+        String name = Main.APP_NAME;
+        assertEquals(name, "Compress");
+    }
+}
