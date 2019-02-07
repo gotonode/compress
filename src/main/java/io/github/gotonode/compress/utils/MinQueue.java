aafd4188c02210eecccd1487299d8b07fa2167d2
gotonode <5214152+gotonode@users.noreply.github.com>
2019-02-07 23:45:39 +0200

    Started work on MinQueue

---
+++ src/main/java/io/github/gotonode/compress/utils/MinQueue.java
@@ -0,0 +1,29 @@
+package io.github.gotonode.compress.utils;
+
+import java.util.PriorityQueue;
+
+/**
+ * This is my own minimum priority queue. Work in progress.
+ *
+ * @param <Type> What type should this queue consist of.
+ */
+public class MinQueue<Type> {
+
+    private PriorityQueue<Type> pq;
+
+    public MinQueue() {
+        pq = new PriorityQueue<>();
+    }
+
+    public void offer(Type type) {
+        pq.offer(type);
+    }
+
+    public int size() {
+        return pq.size();
+    }
+
+    public Type poll() {
+        return pq.poll();
+    }
+}
